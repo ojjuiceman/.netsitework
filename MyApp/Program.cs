6d9938f6c9fb7077b4d3068c74f943eeb5de9bb0
Okera Johnson <okerajohnson@gmail.com>
2022-08-31 17:48:47 -0400

    Initial commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using ServiceStack;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#if DEBUG
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false).AddRazorRuntimeCompilation();
+#else
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+#endif
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseHttpsRedirection();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.UseServiceStack(new AppHost());
+app.UseMvc(routes =>
+{
+    routes.MapRoute(
+        name: "default",
+        template: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
